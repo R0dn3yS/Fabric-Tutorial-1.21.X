14fd7e5d42e0a41b6727f462952ac05c07cf0f4c
Rodney van den Velden <rodneymichael2002@gmail.com>
2025-12-22 07:39:48 +0100

    E7: Advanced Blocks

--- src/main/java/nl/iusearchbtw/tutorialmod/block/ModBlocks.java
+++ src/main/java/nl/iusearchbtw/tutorialmod/block/ModBlocks.java
@@ -13,6 +13,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import nl.iusearchbtw.tutorialmod.block.custom.MagicBlock;
 
 public class ModBlocks {
   public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
@@ -30,6 +31,9 @@ public class ModBlocks {
   public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
     new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
       AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+  public static final Block MAGIC_BLOCK = registerBlock("magic_block",
+    new MagicBlock(AbstractBlock.Settings.create().strength(1f).requiresTool()));
 
 
   private static Block registerBlock(String name, Block block) {
