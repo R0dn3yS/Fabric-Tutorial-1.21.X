23df4a98293ab40b0a3980b2237e63f390224c9a
Rodney van den Velden <rodneymichael2002@gmail.com>
2025-12-22 06:27:38 +0100

    E4: Item Groups

--- src/main/java/nl/iusearchbtw/tutorialmod/TutorialMod.java
+++ src/main/java/nl/iusearchbtw/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package nl.iusearchbtw.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import nl.iusearchbtw.tutorialmod.block.ModBlocks;
+import nl.iusearchbtw.tutorialmod.item.ModItemGroups;
 import nl.iusearchbtw.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,8 @@ public class TutorialMod implements ModInitializer {
 
   @Override
   public void onInitialize() {
+    ModItemGroups.registerItemGroups();
+
     ModItems.registerModItems();
     ModBlocks.registerModBlocks();
   }
