149be6aab8ce045500cd39322b76a3c3f7772060
Rodney van den Velden <rodneymichael2002@gmail.com>
2025-12-22 06:14:21 +0100

    E3: Custom Blocks

--- src/main/java/nl/iusearchbtw/tutorialmod/TutorialMod.java
+++ src/main/java/nl/iusearchbtw/tutorialmod/TutorialMod.java
@@ -2,16 +2,18 @@ package nl.iusearchbtw.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import nl.iusearchbtw.tutorialmod.block.ModBlocks;
 import nl.iusearchbtw.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
