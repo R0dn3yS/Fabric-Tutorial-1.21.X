890232f95bd8d8b9f40c9f6d25a497a07c6d7c59
Rodney van den Velden <rodneymichael2002@gmail.com>
2025-12-23 07:33:41 +0100

    E13: Blockstates

--- src/main/java/nl/iusearchbtw/tutorialmod/block/ModBlocks.java
+++ src/main/java/nl/iusearchbtw/tutorialmod/block/ModBlocks.java
@@ -12,6 +12,7 @@ import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import nl.iusearchbtw.tutorialmod.block.custom.MagicBlock;
+import nl.iusearchbtw.tutorialmod.block.custom.PinkGarnetLampBlock;
 
 public class ModBlocks {
   public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
@@ -55,6 +56,10 @@ public class ModBlocks {
     new DoorBlock(BlockSetType.IRON, AbstractBlock.Settings.create().strength(2f).requiresTool().nonOpaque()));
   public static final Block PINK_GARNET_TRAPDOOR = registerBlock("pink_garnet_trapdoor",
     new TrapdoorBlock(BlockSetType.IRON, AbstractBlock.Settings.create().strength(2f).requiresTool().nonOpaque()));
+
+  public static final Block PINK_GARNET_LAMP = registerBlock("pink_garnet_lamp",
+    new PinkGarnetLampBlock(AbstractBlock.Settings.create()
+      .strength(1f).requiresTool().luminance(state -> state.get(PinkGarnetLampBlock.CLICKED) ? 15 : 0)));
 
 
   private static Block registerBlock(String name, Block block) {
